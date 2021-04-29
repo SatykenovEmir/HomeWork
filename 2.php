6364bc04bb752f56fb48691bc04779cb09d60ec7
SatykenovEmir <74263623+SatykenovEmir@users.noreply.github.com>
2021-04-30 01:11:43 +0600

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<!-- saved from url=(0052)file:///C:\Users\User\Documents\Zoom -->
+<html lang="" dir="" class="">
+<body class="">
+    
+    
+
+
+
+
+
+
+<div class="" id="">
+
+            
+        
+        
+    
+    
+
+
+    
+            
+    
+        
+                
+        
+
+    <div id="" class="">
+                        <section id="" role="">
+
+            
+            
+                                                  <div class="">
+            
+<div id="" class="">
+<div class="">
+          <div class="">
+        
+<article id="" class="" role="">
+  
+                
+    
+<div class="">
+<fieldset class="">
+  
+<div class="">  
+  <a href="file:///C:/Users/User/project%20Jhon/accept/img/Jhon.png"><img src="Jhon.png" width="500" height="415" alt=""></a></div>
+  
+  <p><span class="field-list">Surname:</span>Jhon Armslong</p>  <p><span class="field-list">Nicknames:</span> 	Jhon</p>
+<p></p>    <p><span class="field-list">Personality:</span>unknown</p>  <p><span class="field-list">Paul:</span>Male</p>  <p><span class="field-list">Position:</span>unknown</p>  <p><span class="field-list">Height:</span>170 cm</p>  <p><span class="field-list">Weight:</span>72 kg</p> 
+  
+<p></p>          
+  <p><span class="field-list">Place of Birth:</span>Mercury</p>    
+    <p><span class="field-list">Marital status:</span>  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">the bachelor
+</a></p>          
+</fieldset>  
+  
+</div>
+<center>
+</center>  
+
+
+
+ 
+   
+    
+   
+  
+            
+          
+       
+   		            
+  
+  
+
+      
+      
+      
+   
+    
+
+      
+</article>
+    </div>
+</div>
+</div>
+  </div>
+        </section>
+
+        
+            
+
+            
+                
+                
+
+    </div>
+
+        
+            
+    </div>
+
+
+
+
+
+
+
+
+
+</body></html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jhon</title>
+</head>
+<body>
+    <a href="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fc%2Fc1%2FJohn_Boyne_in_Dublin_(cropped).JPG&f=1&nofb=1"> <img src="Jhon.png" width="250" height="250" alt=""></a>
+    <h2>Jhon Armslong</h2>
+    <h3>Age 18</h3>
+    <h3>Paul Male </h3>
+    <h3>Email emirkrasavchik@gmail.com</h3>
+    <h3>Contact +996500010101</h3>
+    
+    
+</body>
+</html>
